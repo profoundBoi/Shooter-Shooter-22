8198e8dab41e742390c6c12ed4fbcb82832d39f4
profoundBoi <connor170103@gmail.com>
2024-08-16 17:53:59 +0200

    knife pickup

--- Shooter Shooter 22/Assets/Scripts/FirstPersonControl.cs
+++ Shooter Shooter 22/Assets/Scripts/FirstPersonControl.cs
@@ -215,6 +215,7 @@ public class FirstPersonControl : MonoBehaviour
         {
             holdingKnife = false;
             Destroy(heldObject);
+            KnifeCount = 3;
         }
 
         
