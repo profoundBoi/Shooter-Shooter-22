3b2b5bae25965349f36d69f09c9826ffb41bcd2c
lxrdflame <tyrique.s.r@gmail.com>
2024-10-30 16:59:47 +0200

    Crosshead and bat stuff Work better

--- Shooter Shooter 22/Assets/BatScript.cs
+++ Shooter Shooter 22/Assets/BatScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 
 public class BatScript : MonoBehaviour
@@ -8,22 +9,53 @@ public class BatScript : MonoBehaviour
     public Transform[] Spots;
     private Transform nextSpot;
 
+
+    public GameObject Fang;
+
+    [SerializeField]
+    private int HP = 2;
+    public GameObject Blood;
+
     private void Start()
     {
         nextSpot = Spots[Random.Range(0, Spots.Length)];
     }
     private void Update()
     {
-        
-            transform.position = Vector3.MoveTowards(transform.position, nextSpot.position, 9 * Time.deltaTime);
-            Vector3 direction = nextSpot.position - transform.position;
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        transform.position = Vector3.MoveTowards(transform.position, nextSpot.position, 9 * Time.deltaTime);
+        Vector3 direction = nextSpot.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, angle, 0);
 
         if (Vector3.Distance(transform.position, nextSpot.position) < 0.1f)
         {
             // Pick a new random spot
             nextSpot = Spots[Random.Range(0, Spots.Length)];
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            HP--;
+
+            if (HP == 0)
+            {
+                GameObject Bloodtrail = Instantiate(Blood, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(Bloodtrail, 0.5f);
+                int Eye = Random.Range(1, 2);
+                if (Eye == 1)
+                {
+                    Instantiate(Fang, transform.position, Quaternion.identity);
+                }
+
+                Debug.Log(Eye);
+            }
+
         }
     }
 }
